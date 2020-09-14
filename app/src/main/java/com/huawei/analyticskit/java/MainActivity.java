1f7fd073b372fb0cb85bf6119cbe5db23fad7ec9
akopyan757@gmail.com <akopyan.albert@huawei.com>
2020-09-14 16:50:49 +0300

    Update.gitignore file

--- app/src/main/java/com/huawei/analyticskit/java/MainActivity.java
+++ app/src/main/java/com/huawei/analyticskit/java/MainActivity.java
@@ -1,10 +1,8 @@
 package com.huawei.analyticskit.java;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
